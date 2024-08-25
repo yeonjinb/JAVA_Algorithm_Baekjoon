7ba245fd66f1e2dbd1ee431410ab84e573a97e42
yeonjinb <155068491+yeonjinb@users.noreply.github.com>
2024-08-25 19:03:43 +0900

    [Bronze IV] Title: 주사위 세개, Time: 168 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -5,31 +5,18 @@ public class Main{
        int i = sc.nextInt();
        int j = sc.nextInt();
        int k = sc.nextInt();
-       int prize;
        
        if (i==j && i==k) {
-    	   prize = 10000 + i*1000;
+    	   System.out.println(10000 + i*1000);
        }
-       else if (i==j) {
-    	   prize = 1000 + i*100;
+       else if (i==j || i==k) {
+    	   System.out.println(1000 + i*100);
        }
        else if (j==k) {
-    	   prize = 1000 + j*100;
-       }
-       else if (k==i) {
-    	   prize = 1000 + k*100;
+    	   System.out.println(1000 + j*100);
        }
        else {
-    	   int temp = i;
-    	   if (temp < j) {
-    		   temp = j;
-    	   }
-    	   if (temp < k) {
-    		   temp = k;
-    	   }
-    	   prize = temp*100;
+    	   System.out.println((Math.max(Math.max(i, j), k)*100));
        }
-       
-       System.out.println(prize);
     }  
 }
